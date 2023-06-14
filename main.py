dc7f0e08676f6d0f1076141d39980ba3d4e7d8fe
SerheyBublik <zf.41@tut.by>
2023-06-14 10:29:57 +0300

    add коментарии к функциям

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ PATH = 'data/operations.json'
 
 def convert_date(date):
     """
+    Дата перевода представлена в формате ДД.ММ.ГГГГ
     example 2019-12-07T06:17:14.634890 -> 07.12.2019
     """
     return '.'.join(date[:10].split('-')[::-1])
 
 
 def masking_card(card_info):
+    """
+    - Номер карты замаскирован и не отображается целиком в формате  XXXX XX** **** XXXX (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
+- Номер счета замаскирован и не отображается целиком в формате  **XXXX
+(видны только последние 4 цифры номера счета).
+    """
     card_info = card_info.split()
     number = card_info[-1]
     if len(number) == 16:
@@ -25,14 +31,29 @@ def masking_card(card_info):
 
 
 def show_operation(operation):
+    """
+    выводит  операци в формате:
+    <дата перевода> <описание перевода>
+<откуда> -> <куда>
+<сумма перевода> <валюта>
+# Пример вывода для одной операции:
+14.10.2018 Перевод организации
+Visa Platinum 7000 79** **** 6361 -> Счет **9638
+82771.72 руб.
+    """
     out = f"""{convert_date(operation['date'])} Перевод организации
      {masking_card(operation['from'])} -> {masking_card(operation['to'])}
-{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}."""
+{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}.
+"""
 
     print(out)
 
 
 def main():
+    """
+    Открывает файл, выбирает "EXECUTED" операции, сортирует по дате, выбирает 5 последних
+    :return: show_operation(operation)
+    """
     try:
         with open(PATH, mode='r', encoding='utf8') as file:
             data = json.load(file)
